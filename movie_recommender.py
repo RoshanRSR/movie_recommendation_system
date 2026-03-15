ff044376833d0e3061f9583fe48ccb1c7213a5bc
Roshan <rashnm.kan@gmail.com>
2026-03-16 00:07:59 +0530

    Movie Recommendation System

---
+++ movie_recommender.py
@@ -0,0 +1,28 @@
+import pandas as pd
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
+
+# Load dataset 
+movies = pd.read_csv("dataset/tmdb_5000_movies.csv")
+# select column
+movies = movies[['title','overview']]
+#remove blank/missing value
+movies.dropna(inplace=True)
+#convert text into vectors
+vectorizer = CountVectorizer(stop_words='english')
+matrix = vectorizer.fit_transform(movies['overview'])
+
+#Similarity matrix
+similarity = cosine_similarity(matrix)
+
+def recommend(movie_name):
+  movie_index = movies[movies['title']==movie_name].index[0]
+  distances = similarity[movie_index]
+  movie_list = sorted(list(enumerate(distances)), reverse=True,key=lambda x:x[1])[1:6]
+  for i in movie_list:
+    print(movies.iloc[i[0]].title)
+
+# call the function for the test
+user_movie_name = input("Enter your favorite movie : ")
+
+recommend(user_movie_name)
